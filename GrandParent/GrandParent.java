21b0600aaa0d222a08e0777193b6096fc9b763a2
blockenters <58394003+blockenters@users.noreply.github.com>
2020-05-13 15:59:08 +0900

    a

---
+++ GrandParent/GrandParent.java
@@ -0,0 +1,16 @@
+package com.example.libtest1;
+
+public class GrandParent {
+    protected String name;
+    protected int age;
+
+    public GrandParent(){
+        System.out.println("GrandParent class");
+    }
+
+    public void printGrandParentInfo(){
+        System.out.println("name : " + name);
+        System.out.println("age : " + age);
+    }
+
+}
