70e57aa93acbe07c6dc82c28627bf6fe7b8b3361
blockenters <58394003+blockenters@users.noreply.github.com>
2020-05-13 15:31:18 +0900

    a

--- Person/MyClass.java
+++ Person/MyClass.java
@@ -1,10 +1,18 @@
 package com.example.libtest1;
 
-import java.awt.MenuBar;
-
 public class MyClass {
 
     public static void main(String[] args) {
+        Professor f = new Professor();
+        f.setNum(2);
+        f.setName("Mike");
+        f.setDept("Computer");
+        f.setAddress("Seoul");
+        String[] sub = {"computer","C","Java"};
+        f.setSubjects(sub);
+
+        f.print();
+        f.print_subject();
 
     }
 }
