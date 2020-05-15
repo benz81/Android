b8d70023a0f5964bd2ac01bf5489225d3c4c6af2
blockenters <58394003+blockenters@users.noreply.github.com>
2020-05-15 16:35:24 +0900

    a

---
+++ 1_1_실습문제코드/09_Class 설계 총정리/ClassTest_1.java
@@ -0,0 +1,33 @@
+package com.example.libtest1;
+
+public class ClassTest_1 {
+
+    private String name;
+    private int salary;
+
+    public ClassTest_1(){
+
+    }
+    ClassTest_1(String n, int s){
+        name = n;
+        salary = s;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public void getInformation(){
+        System.out.println("name : "+name + " Salary : "+salary);
+    }
+    public void prn(){
+        System.out.println("super class");
+    }
+
+
+
+
+
+
+}
---
+++ 1_1_실습문제코드/09_Class 설계 총정리/ClassTest_2.java
@@ -0,0 +1,19 @@
+package com.example.libtest1;
+
+public class ClassTest_2 extends ClassTest_1{
+
+    String department;
+
+    ClassTest_2(String name, int salary, String department){
+        super(name, salary);
+        this.department = department;
+    }
+
+    @Override
+    public void getInformation() {
+        String name = super.getName();
+        int salary = super.getSalary();
+        System.out.println("name : "+name +", salary : "+salary
+                + ", dep : "+department);
+    }
+}
