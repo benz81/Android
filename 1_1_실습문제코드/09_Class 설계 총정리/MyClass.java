86d67b87c57bdb9c911ec63224a522c91ed8e02f
blockenters <58394003+blockenters@users.noreply.github.com>
2020-05-15 17:56:19 +0900

    a

---
+++ 1_1_실습문제코드/09_Class 설계 총정리/MyClass.java
@@ -0,0 +1,18 @@
+package com.example.libtest1;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        ClassTest_2 ct1 = new ClassTest_2("Mike", 3000, "Edu");
+        ct1.getInformation();
+
+    }
+}
+
+
+
+
+
+
+
