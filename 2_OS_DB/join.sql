40491581bddb1dff7d733edc5324b0424a25132f
blockenters <58394003+blockenters@users.noreply.github.com>
2020-05-22 13:10:27 +0900

    a

--- 2_OS_DB/join.sql
+++ 2_OS_DB/join.sql
@@ -28,6 +28,88 @@ from girl_group as g
 join song as s
 on g.hit_song_id = s._id;
 
+-- 가수 이름, 데뷔날짜를 조회하세요.
+select name, debut from girl_group;
+
+-- 데뷔날짜가 2008년1월17일인 그룹의 이름과 데뷔날짜를 조회하세요. 
+select name, debut
+from girl_group
+where debut = "2008-01-17";
+
+-- 데뷔년도가 2009년 1월 1일 이후에 데뷔한 그룹의 
+-- 이름과, 데뷔날짜, 노래제목을 조회하세요.
+select g.name, g.debut, s.title
+from girl_group as g
+join song as s
+on g.hit_song_id = s._id
+where g.debut > "2009-01-01";
+
+-- 이름에 "스" 자가 들어가는 걸그룹의
+-- 걸그룹이름과, 데뷔날짜, 타이틀을 조회하세요.
+select g.name, g.debut, s.title
+from girl_group as g
+join song as s
+on g.hit_song_id = s._id
+where g.name like "%스%" ;
+
+-- 데뷔년도가 2008년 5월 1일 이후에 데뷔한 그룹의 
+-- 이름과, 데뷔날짜, 노래제목을 최근 데뷔한 걸그룹부터 조회하세요.
+select g.name, g.debut, s.title
+from girl_group as g
+join song as s
+on g.hit_song_id = s._id
+where g.debut > "2008-05-01"
+order by g.debut desc;
+
+
+--
+
+select g.*, s.* 
+from girl_group as g
+join song as s
+on g.hit_song_id = s._id;
+
+-- left outer join
+select g.*, s.*
+from girl_group as g
+left join song as s
+on g.hit_song_id = s._id;
+
+select s.*, g.*
+from song as s
+left join girl_group as g
+on  s._id = g.hit_song_id;
+
+-- 걸그룹 중에서 히트곡(노래 타이틀)이 없는 걸그룹의 
+-- 걸그룹 이름과 데뷔날짜를 조회하세요.
+select g.name, g.debut
+from girl_group as g
+left join song as s
+on  s._id = g.hit_song_id
+where s.title is NULL;
+
+-- 노래 피노키오를 부른 가수의 이름을 조회하세요.
+select g.name
+from song as s
+left join girl_group as g
+on  s._id = g.hit_song_id
+where s.title = "피노키오";
+-- 노래 기대해 를 부른 가수의, 노래제목과 이름을 조회하세요.
+select s.title, g.name
+from song as s
+left join girl_group as g
+on  s._id = g.hit_song_id
+where s.title = "기대해";
+
+
+
+
+
+
+
+
+
+
 
 
 
