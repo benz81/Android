a15445ff8e67813e35a82d2ddaebdbe6cddff65d
blockenters <58394003+blockenters@users.noreply.github.com>
2020-05-22 16:06:39 +0900

    a

--- 2_OS_DB/join.sql
+++ 2_OS_DB/join.sql
@@ -101,6 +101,64 @@ left join girl_group as g
 on  s._id = g.hit_song_id
 where s.title = "기대해";
 
+-- emp_test 데이터베이스로 이동.
+use emp_test;
+
+-- DALLAS에서 근무하는 사원의 이름, 직위, 부서번호, 부서이름 조회
+select e.ename, e.job, e.deptno, d.dname 
+from emp as e
+join dept as d
+on e.deptno = d.deptno
+where d.loc = "DALLAS";
+
+
+-- 이름에 A 가 들어가는 사원들의 이름과 부서이름을 조회하세요.
+select e.ename, d.dname 
+from emp as e
+join dept as d
+on e.deptno = d.deptno
+where e.ename like "%A%";
+
+-- 사원이름과 그 사원이 속한 부서의 부서명, 그리고 월급을 조회.
+-- 단, 월급이 3000 이상인 사원 대상으로.
+select  e.ename, d.dname, e.sal
+from emp as e
+join dept as d
+on e.deptno = d.deptno
+where e.sal >= 3000;
+
+-- 직위가 SALESMAN 인 사원들의, 
+-- 직위, 사원이름, 부서명 조회
+select  e.job, e.ename, d.dname 
+from emp as e
+join dept as d
+on e.deptno = d.deptno
+where e.job = "SALESMAN";
+
+-- 모든 사원들의 사원번호, 이름, 연봉, 연봉+커미션,
+-- 급여등급을 조회하되,
+-- 각각의 컬럼명을, 한글로 사원번호, 사원이름, 연봉, 실급여, 급여등급
+select e.empno as 사원번호, e.ename 사원이름, ifnull(e.comm,0),
+	e.sal as 연봉, e.sal + ifnull(e.comm , 0) as 실급여 , 
+    s.grade as 급여등급   
+from emp as e
+join salgrade as s
+on e.sal between s.losal and s.hisal;
+
+-- 커미션이 있는 사원만
+select e.empno as 사원번호, e.ename 사원이름, 
+	e.sal as 연봉, e.sal + e.comm as 실급여 , 
+    s.grade as 급여등급   
+from emp as e
+join salgrade as s
+on e.sal between s.losal and s.hisal
+where e.comm is not null;
+
+-- 부서번호가 10번인 사원들의 부서번호, 부서이름, 사원이름, 
+-- 월급, 급여등급을 출력하시오.
+
+
+
 
 
 
