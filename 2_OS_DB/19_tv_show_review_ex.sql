37299d9813d0401fc1ac8e203266a3badd979119
blockenters <58394003+blockenters@users.noreply.github.com>
2020-05-29 10:22:33 +0900

    a

--- 2_OS_DB/19_tv_show_review_ex.sql
+++ 2_OS_DB/19_tv_show_review_ex.sql
@@ -30,7 +30,35 @@ join reviews as rv
 on s.id = rv.series_id
 group by s.genre;
 
+use mydb;
 
+
+-- 리뷰 작성을 안한 사람도 표시하되(0으로), 사람별로 각 리뷰의 갯수,
+-- 리뷰의 최소별점(rating), 최대별점, 평균별점을 조회하고,
+-- 리뷰 갯수가 0보다 크면, "ACTIVE", 그렇지 않으면 "INACTIVE"로 
+-- 표시 ( 컬럼명은 status) 
+
+-- first_name, lst_name, count, min, max, avg, status
+select r.first_name, r.last_name, count(rv.rating) as count,
+	round(min( ifnull(rv.rating,0)),2) as min, 
+    round(max(ifnull(rv.rating,0)),2) as max, 
+    round(avg( ifnull(rv.rating,0)) , 2) as avg,
+    if( count(rv.rating) > 0, "ACTIVE", "INACTIVE") as status
+from reviewers as r
+left join reviews as rv
+on r.id = rv.reviewer_id
+group by r.id ;
+
+-- 영화제목으로 알파벳 오름차순 정렬해서, 
+-- 영화제목, 별점, 리뷰작성자이름(합쳐서) 조회하시오.
+select s.title, rv.rating, 
+	concat( r.first_name, " ",r.last_name) as reviewer
+from reviewers as r
+join reviews as rv
+on r.id = rv.reviewer_id
+join series as s
+on s.id = rv.series_id
+order by s.title;
 
 
 
