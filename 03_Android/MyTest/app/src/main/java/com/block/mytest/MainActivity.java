576303e43c913dc00221a748479931e4944f9736
blockenters <58394003+blockenters@users.noreply.github.com>
2020-06-05 17:56:57 +0900

    a

--- 03_Android/MyTest/app/src/main/java/com/block/mytest/MainActivity.java
+++ 03_Android/MyTest/app/src/main/java/com/block/mytest/MainActivity.java
@@ -1,14 +1,49 @@
 package com.block.mytest;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtTitle;
+    EditText editName;
+    EditText editPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTitle = findViewById(R.id.txtTitle);
+        editName = findViewById(R.id.editName);
+        editPhone = findViewById(R.id.editPhone);
+
+        // 버튼을 클릭하면, 로그에 "버튼 클릭 됨" 을 찍도록 코드 작성
+        Button btnClick = findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyTest", "버튼 클릭 됨");
+                String text = txtTitle.getText().toString();
+                Log.i("MyTest", "get test : " + text);
+
+                String name = editName.getText().toString();
+                String phone = editPhone.getText().toString();
+
+                txtTitle.setText(name + "\n" + phone);
+
+                Toast.makeText(MainActivity.this, name+"\n"+phone,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
