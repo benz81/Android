30fc23988972db0f8c8988189a7e547b54d64d74
blockenters <58394003+blockenters@users.noreply.github.com>
2020-06-24 16:07:55 +0900

    A

--- 03_Android/Serializable/app/src/main/java/com/block/serializable/Second.java
+++ 03_Android/Serializable/app/src/main/java/com/block/serializable/Second.java
@@ -3,12 +3,30 @@ package com.block.serializable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.block.serializable.model.Person;
 
 public class Second extends AppCompatActivity {
+
+    TextView txtPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        txtPerson = findViewById(R.id.txtPerson);
+
+        Person person = (Person) getIntent().getSerializableExtra("PersonClass");
+
+        String info = person.getName() + ", " + person.getEmail() + ", " + person.isMale();
+
+        txtPerson.setText(info);
+
     }
 }
+
+
+
+
