1572de0aecf41594fd9bf6d593c50b6ec494c96e
blockenters <58394003+blockenters@users.noreply.github.com>
2020-06-26 17:34:26 +0900

    aa

--- 03_Android/Youtube/app/src/main/java/com/block/youtube/MainActivity.java
+++ 03_Android/Youtube/app/src/main/java/com/block/youtube/MainActivity.java
@@ -3,10 +3,23 @@ package com.block.youtube;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    String youtubeUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBbbw95I-YTfS09K3rNpff6rSGMFETyOhQ&maxResults=20&order=date&type=video";
+    String youtubeUrl = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyBbbw95I-YTfS09K3rNpff6rSGMFETyOhQ&maxResults=20&order=date&type=video&q=벤츠";
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +28,42 @@ public class MainActivity extends AppCompatActivity {
 
         // 발리 라이브러리 이용해서, 호출
         // 로그 찍어본다.
-        
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                youtubeUrl,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("AAA", response.toString());
+                        try {
+                            JSONArray items = response.getJSONArray("items");
+                            for(int i = 0; i < items.length(); i++){
+                                JSONObject jsonObject = items.getJSONObject(i);
+                                JSONObject snippet = jsonObject.getJSONObject("snippet");
+                                String title = snippet.getString("title");
+                                String desc = snippet.getString("description");
+                                JSONObject thumbnails = snippet.getJSONObject("thumbnails");
+                                JSONObject def = thumbnails.getJSONObject("default");
+                                String url = def.getString("url");
+
+                                Log.i("AAA", title+", "+desc+", "+url);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonObjectRequest);
 
     }
 }
