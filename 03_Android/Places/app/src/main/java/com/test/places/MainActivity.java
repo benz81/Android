e4e522b15da722846c3a65cbe4c2bb0ee8333724
blockenters <58394003+blockenters@users.noreply.github.com>
2020-07-03 16:33:11 +0900

    a

--- 03_Android/Places/app/src/main/java/com/test/places/MainActivity.java
+++ 03_Android/Places/app/src/main/java/com/test/places/MainActivity.java
@@ -2,9 +2,12 @@ package com.test.places;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -61,14 +64,24 @@ public class MainActivity extends AppCompatActivity {
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
-                  Log.i("AAA", location.toString());
-                  double lat = location.getLatitude();
-                  double lng = location.getLongitude();
-                  // 네트워크로 구글 플레이스 api 호출.
+                Log.i("AAA", location.toString());
+                double lat = location.getLatitude();
+                double lng = location.getLongitude();
+                // 네트워크로 구글 플레이스 api 호출.
                 getNetworkData(lat, lng);
             }
         };
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 유저한테, 이 앱은 위치기반 권한이 있어야 한다고 알려야 한다.
+            // 유저가 권한 설정을 하고 나면, 처리해야 할 코드를 작성하기 위해서,
+            // requestCode 값을 설정한다.
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                5000, 0, locationListener);
     }
 
     void getNetworkData(double lat, double lng) {
@@ -89,7 +102,14 @@ public class MainActivity extends AppCompatActivity {
                                 JSONObject location = geometry.getJSONObject("location");
                                 double storeLat = location.getDouble("lat");
                                 double storeLng = location.getDouble("lng");
+                                String name = item.getString("name");
+                                String addr = item.getString("vicinity");
+                                Store store = new Store(name, addr, storeLat, storeLng);
+                                storeArrayList.add(store);
                             }
+                            adapter = new RecyclerViewAdapter(MainActivity.this,
+                                    storeArrayList);
+                            recyclerView.setAdapter(adapter);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -105,4 +125,35 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 0){
+            if(ActivityCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                            PackageManager.PERMISSION_GRANTED){
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    5000,   // 밀리세컨드,  1000 : 1초
+                    0,   // 미터   10m
+                    locationListener);
+        }
+    }
+
 }
+
+
+
+
+
+
