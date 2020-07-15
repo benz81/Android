654a89ffde097150d6dc5287287e01474b68c8c0
blockenters <58394003+blockenters@users.noreply.github.com>
2020-07-15 12:20:23 +0900

    Update app.js

--- 08_node/employee/app.js
+++ 08_node/employee/app.js
@@ -8,30 +8,28 @@ let path = '/api/v1/employees'
 
 request.get( {url:  baseUrl+path, json:true} , 
     function(error, response, body){
-        //console.log(body.data)
-
-        // let query = 'insert into employee (name, salary,age) \
-        // values ("'+body.data[0].employee_name+'", '+
-        // body.data[0].employee_salary+', '+
-        // body.data[0].employee_age+');'
         
         let array = body.data
-
-        let query = 'insert into employee (name, salary, age) values '
         
+        let query = 'insert into employee (name, salary, age) values ? '
+        // ? 에 들어갈 데이터는 [  ] 로 만들어야 합니다.
+        let data = []
         for(let i = 0; i < array.length; i++){
-            query = query + `("${array[i].employee_name}" , 
-                                ${array[i].employee_salary}, 
-                                ${array[i].employee_age}),`
+            data.push( [array[i].employee_name, 
+                        array[i].employee_salary,
+                        array[i].employee_age] )
         }
-        query = query.slice(0, -1)
-        console.log(query)
-
-        connection.query(query, function(error, results, fields){
-            console.log(results)
+        console.log(data)
+        connection.query(query, [data] , function(error, results, fields){
+                console.log(results)
         })
-        connection.end()
+        connection.end()        
     });
+
+
+
+
+
 
 
 // 인서트 문 하나로, 여러 문장을 한번에 집어 넣는 방법.
