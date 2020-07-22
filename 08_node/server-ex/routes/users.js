868c79eb21439600d99b2baa6c5d28a056e73ecd
blockenters <58394003+blockenters@users.noreply.github.com>
2020-07-22 13:02:08 +0900

    a

--- 08_node/server-ex/routes/users.js
+++ 08_node/server-ex/routes/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { createUser } = require("../controllers/users");
+const { createUser, loginUser, changePasswd } = require("../controllers/users");
 
 const router = express.Router();
 
 router.route("/").post(createUser);
+router.route("/login").post(loginUser);
+router.route("/change").post(changePasswd);
 
 module.exports = router;
