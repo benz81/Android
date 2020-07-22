618e8772a58dbf1c3b95ffd440e4d4dd28121f39
blockenters <58394003+blockenters@users.noreply.github.com>
2020-07-22 12:56:20 +0900

    a

--- 08_node/server-ex/controllers/users.js
+++ 08_node/server-ex/controllers/users.js
@@ -42,3 +42,71 @@ exports.createUser = async (req, res, next) => {
     }
   }
 };
+
+// 로그인 api를 개발하세요.
+
+// @desc    로그인
+// @route   POST /api/v1/users/login
+// @parameters  {"email" : "hello@gmail.com" , "passwd":"1234"}
+exports.loginUser = async (req, res, next) => {
+  let email = req.body.email;
+  let passwd = req.body.passwd;
+
+  let query = "select * from user where email = ? ";
+  let data = [email];
+
+  try {
+    [rows] = await connection.query(query, data);
+    // 디비에 저장된 비밀번호 가져와서
+    let savedPasswd = rows[0].passwd;
+    // 비밀번호 체크 : 비밀번호가 서로 맞는지 확인
+    let isMatch = await bcrypt.compare(passwd, savedPasswd);
+    // let isMatch = bcrypt.compareSync(passwd, savedPasswd);
+
+    res.status(200).json({ success: true, result: isMatch });
+  } catch (e) {
+    res.status(500).json({ success: false, error: e });
+  }
+};
+
+// @desc    패스워드 변경
+// @route   POST /api/v1/users/change
+// @parameters  email, passwd, new_passwd
+exports.changePasswd = async (req, res, next) => {
+  let email = req.body.email;
+  let passwd = req.body.passwd;
+  let new_passwd = req.body.new_passwd;
+
+  // 이 유저가, 맞는 유저인지 체크
+  let query = "select passwd from user where email = ?";
+  let data = [email];
+
+  try {
+    [rows] = await connection.query(query, data);
+    let savedPasswd = rows[0].passwd;
+
+    let isMatch = bcrypt.compareSync(passwd, savedPasswd);
+
+    if (isMatch != true) {
+      res.status(401).json({ success: false, result: isMatch });
+      return;
+    }
+  } catch (e) {
+    res.status(500).json({ success: false, error: e });
+  }
+
+  query = "update user set passwd = ? where email = ? ";
+  const hashedPasswd = await bcrypt.hash(new_passwd, 8);
+  data = [hashedPasswd, email];
+
+  try {
+    [result] = await connection.query(query, data);
+    if (result.affectedRows == 1) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(200).json({ success: false });
+    }
+  } catch (e) {
+    res.status(500).json({ success: false, error: e });
+  }
+};
