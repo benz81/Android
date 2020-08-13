eb418e0e989640184c93d5b5684324160f177219
blockenters <58394003+blockenters@users.noreply.github.com>
2020-08-13 12:05:09 +0900

    aa

---
+++ 13_UI구현/PhotoApp/app/src/main/java/com/block/photoapp/utils/Utils.java
@@ -0,0 +1,7 @@
+package com.block.photoapp.utils;
+
+public class Utils {
+
+    public static final String BASE_URL = "http://contactserver5-env.eba-5yvw6x6m.ap-northeast-2.elasticbeanstalk.com";
+    public static final String PREFERENCES_NAME = "photo_app";
+}
